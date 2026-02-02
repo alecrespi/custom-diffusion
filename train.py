6e4080ad492f160a57aae24a089cea19fd717edf
Alessandro Crespi <alessandro.crespi.2002@gmail.com>
2026-02-02 16:51:28 +0100

    prevent ckpting every epoch (forced)

--- train.py
+++ train.py
@@ -826,9 +826,13 @@ if __name__ == "__main__":
         }
         if hasattr(model, "monitor"):
             print(f"Monitoring {model.monitor} as checkpoint metric.")
-            default_modelckpt_cfg["params"]["monitor"] = model.monitor
-            default_modelckpt_cfg["params"]["save_top_k"] = -1
-            default_modelckpt_cfg["params"]["every_n_epochs"] = 1
+            # default_modelckpt_cfg["params"]["monitor"] = model.monitor
+            # default_modelckpt_cfg["params"]["save_top_k"] = -1
+            # default_modelckpt_cfg["params"]["every_n_epochs"] = 1
+            default_modelckpt_cfg["params"]["monitor"] = None
+            default_modelckpt_cfg["params"]["save_top_k"] = 0
+            default_modelckpt_cfg["params"]["every_n_epochs"] = 0
+            default_modelckpt_cfg["params"]["save_last"] = 1
 
         if "modelcheckpoint" in lightning_config:
             modelckpt_cfg = lightning_config.modelcheckpoint
